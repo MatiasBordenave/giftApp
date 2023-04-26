e60c63bf696f1d39501a88a5f4f5636a8c9b27c4
Matias <bordenavetuc@gmail.com>
2023-04-25 22:16:54 -0300

    gifExpertApp Origianl

---
+++ 04-gift-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+import { Nav } from "./Nav";
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch'])
+
+  const onAddCategory= (newCategory) =>{
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  }
+  
+
+  return (
+    <>
+        <Nav/>
+
+        <h1>Gif Expert App</h1>
+
+        <AddCategory
+          onNewCategory = { (value) =>onAddCategory (value) }
+        />    
+
+        {
+          categories.map(category =>
+              (
+                <GifGrid 
+                  key={ category }
+                  category= { category }
+                />
+              ))
+        }
+    </>
+  )
+}
---
+++ 04-gift-expert-app/src/Nav.jsx
@@ -0,0 +1,14 @@
+
+export const Nav = () => {
+    return (
+        <>
+        <nav className="nav">
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
